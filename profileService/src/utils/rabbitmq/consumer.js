75efdcd0dcaa9c3ba0b99a62a273d3a44c98453f
Suhaas Dhakshana Murthy <suhaasdmurthy@gmail.com>
2026-04-18 22:56:15 +0530

    fixed profile page landing after signing up

--- profileService/src/utils/rabbitmq/consumer.js
+++ profileService/src/utils/rabbitmq/consumer.js
@@ -1,5 +1,4 @@
 const { getChannel } = require("./connection");
-const Profile = require("../../models/profileModel");
 
 async function consumeAuthUserCreated() {
   const channel = getChannel();
@@ -24,22 +23,7 @@ async function consumeAuthUserCreated() {
 
         const { userId, email, firstName, lastName } = data;
 
-        const existingProfile = await Profile.findOne({ userId });
-
-        if (existingProfile) {
-          console.log("Profile already exists:", userId);
-          channel.ack(msg);
-          return;
-        }
-
-        await Profile.create({
-          userId,
-          email,
-          firstName,
-          lastName,
-        });
-
-        console.log("Profile created for user:", userId);
+        console.log("Auth user created, profile will be completed on first save:", userId);
 
         channel.ack(msg);
       } catch (err) {
